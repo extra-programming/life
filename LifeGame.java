c8f7e4fd79d5d7ed3f1035bf61602c2498260e51
1000000000 <lthyde@gmail.com>
2011-11-18 13:00:35 -0500

    Run Performance improvement
    LifeGame implements Runnable and it now checks a boolean to see if it
    should be stepping

--- LifeGame.java
+++ LifeGame.java
@@ -15,11 +15,10 @@ import java.util.*;
 import java.io.*;
 import java.applet.*;
 
-
 /**
  * @author Mike Roam
  */
-public class LifeGame extends JApplet /* was Applet */ {
+public class LifeGame extends JApplet implements Runnable /* was Applet */ {
 
     private static final long serialVersionUID = 42L;  // makes serializable happy
 
@@ -27,7 +26,6 @@ public class LifeGame extends JApplet /* was Applet */ {
     /* static */ Container myContentPane = null; // for applets and standalone
     public static final int FRAMEWIDTH = 500;
     public static final int FRAMEHEIGHT = 500;
-
 
     LifeBoard theBoard = null;  
     /* uh-oh: this is JPanel within my borderLayout, 
@@ -47,35 +45,20 @@ public class LifeGame extends JApplet /* was Applet */ {
 
     JButton quitBtn = new JButton( "Quit" );
     //Button quitBtn = new Button( "Quit" );
-    
+
     JSlider delaySlider = null; /* is instantiated in buildDelayPanel( ) */
-    
-    Thread stepperThread = new Thread(){
-            public void run() {
-                while(true) {
-                    if ( stepContinuouslyBtn.getText().equals("Pause") ) {
-                    	lifeStep(1);
-                    }
-                    try {
-                        sleep( delaySlider.getValue() );
-                    } catch( Exception e ) {
-                        e.printStackTrace( System.err );
-                    } /* end of try */
-                } /* end of while */
-            } /* run( ) */
-        }; /* instance stepperThread */
 
+    boolean running = false;
 
-
+    Thread stepperThread = new Thread(this);
 
     /**
-    * default constructor, called by main
-    */
+     * default constructor, called by main
+     */
     public LifeGame( ) {
         myContentPane = this.getContentPane( );
         init( );
     } // end of default constructor
-
 
     public static void main(String args[]) {
         LifeGame myLifeGame = new LifeGame( );
@@ -83,28 +66,25 @@ public class LifeGame extends JApplet /* was Applet */ {
         frame.getContentPane().add( myLifeGame );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         /* myLifeGame.addWindowListener( new WindowAdapter() {
-                public void windowClosing(WindowEvent e) {
-                    System.exit(0);
-                }
-            }); 
-        */
+        public void windowClosing(WindowEvent e) {
+        System.exit(0);
+        }
+        }); 
+         */
         frame.setSize( FRAMEWIDTH, FRAMEHEIGHT );
         // myLifeGame.init( ); the constructor calls init!
         myLifeGame.start( ); // what's this??
         frame.setVisible(true);
     } // main( )
 
-
-
-
     /**
-    * For starting up Applet
-    * (Also called by main and constructor so standalone acts like applet)
-    */
+     * For starting up Applet
+     * (Also called by main and constructor so standalone acts like applet)
+     */
     public void init( ) {
-//      myContentPane.add( "Center", this ); // necessary?
+        //      myContentPane.add( "Center", this ); // necessary?
         if (myContentPane == null) {
-//          // so I guess we're an applet
+            //          // so I guess we're an applet
             myContentPane = getContentPane( );
         }
         /* Start life with a random board */
@@ -112,22 +92,21 @@ public class LifeGame extends JApplet /* was Applet */ {
         this.buildGUI( );
     } // init( )
 
-    
     public int getBoardX(int x){
         return x;
     }
-    
+
     public int getBoardY(int y){
         return y;
     }
-    
+
     public void changeCellVal(int x, int y){
-        
+
     }
 
     /**
-    * applets and standalones call constructor which calls init which calls this.
-    */
+     * applets and standalones call constructor which calls init which calls this.
+     */
     public void buildGUI() {
         // this.setLayout( new BorderLayout( ) ); / /JApplet have this by default
         JPanel setupPanel = new JPanel();
@@ -138,7 +117,7 @@ public class LifeGame extends JApplet /* was Applet */ {
         setupPanel.setBackground( Color.BLUE );
         setupPanel.add( getFileBtn );
         setupPanel.add( randomBoardBtn );
-        
+
         JPanel controlPanel = new JPanel( );
         controlPanel.setBorder(BorderFactory.createEmptyBorder( /* TLRB */ 10, 10, 10, 10) );
         /* using default flowLayout */
@@ -150,7 +129,7 @@ public class LifeGame extends JApplet /* was Applet */ {
         controlPanel.add( stepPanel );
         controlPanel.add( buildDelayPanel( ) );
         controlPanel.add( quitBtn );
-        
+
         //this.getContentadd( "North", paramPanel );
         if (myContentPane == null) {
             System.out.println("myContentPane is still null");
@@ -160,86 +139,83 @@ public class LifeGame extends JApplet /* was Applet */ {
         myContentPane.add( theBoard, BorderLayout.CENTER);
         //myContentPane.add("East", paramPanel);
         myContentPane.add( setupPanel, BorderLayout.LINE_END);
-        
+
         myContentPane.add( controlPanel, BorderLayout.PAGE_END );
-        
+
         addActionListenersToMyButtons( );
-        
+
         stepperThread.start();
     } // buildGUI( )
 
-
     void addActionListenersToMyButtons( ) {
-        
         this.addMouseListener (new MouseAdapter(){
-            public void mouseClicked(MouseEvent e){
-                int x = e.getX(), y = e.getY();
-                System.out.println("mouse clicked on "+x+", "+y);
-                int bx=getBoardX(x), by=getBoardY(y);
-                if(bx!=-1&&by!=-1) changeCellVal(getBoardX(x),getBoardY(y));
-            }
-        });
-        
+                public void mouseClicked(MouseEvent e){
+                    int x = e.getX(), y = e.getY();
+                    System.out.println("mouse clicked on "+x+", "+y);
+                    int bx=getBoardX(x), by=getBoardY(y);
+                    if(bx!=-1&&by!=-1) changeCellVal(getBoardX(x),getBoardY(y));
+                }
+            });
+
         getFileBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("getFileBtn");
-                myContentPane.remove(theBoard);
-                newBoardFromFile( );
-                // no, bad! myContentPane.removeAll();
-                myContentPane.add( theBoard, BorderLayout.CENTER);
-                myContentPane.validate(); 
-                myContentPane.repaint();
-            }
-        }); // end of addActionListener
-        
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("getFileBtn");
+                    myContentPane.remove(theBoard);
+                    newBoardFromFile( );
+                    // no, bad! myContentPane.removeAll();
+                    myContentPane.add( theBoard, BorderLayout.CENTER);
+                    myContentPane.validate(); 
+                    myContentPane.repaint();
+                }
+            }); // end of addActionListener
+
         randomBoardBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("randomBoard");
-                myContentPane.remove(theBoard);
-                
-                theBoard = new LifeBoard( 16, 16 );
-                theBoard.setBackground( Color.GREEN );
-                // no, bad! myContentPane.removeAll();
-                myContentPane.add( theBoard, BorderLayout.CENTER);
-                //theBoard.paintImmediately(theBoard.getVisibleRect());
-                myContentPane.validate();
-                //myContentPane.repaint();
-                stepContinuouslyBtn.setText("Run");
-                /* [ ] ?? we should have variable for what kind of stepping !! */
-                repaint();
-            }
-        }); // end of addActionListener
-        
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("randomBoard");
+                    myContentPane.remove(theBoard);
+
+                    theBoard = new LifeBoard( 16, 16 );
+                    theBoard.setBackground( Color.GREEN );
+                    // no, bad! myContentPane.removeAll();
+                    myContentPane.add( theBoard, BorderLayout.CENTER);
+                    //theBoard.paintImmediately(theBoard.getVisibleRect());
+                    myContentPane.validate();
+                    //myContentPane.repaint();
+                    stepContinuouslyBtn.setText("Run");
+                    /* [ ] ?? we should have variable for what kind of stepping !! */
+                    repaint();
+                }
+            }); // end of addActionListener
+
         stepOneBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("step");
-                lifeStep( 1 );
-            }
-        }); // end of addActionListener
-        
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("step");
+                    lifeStep( 1 );
+                }
+            }); // end of addActionListener
+
         stepTenBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("step10");
-                lifeStep( 10 );
-            }
-        }); // end of addActionListener
-        
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("step10");
+                    lifeStep( 10 );
+                }
+            }); // end of addActionListener
+
         stepContinuouslyBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("stepContinuously");
-                stepContinuously();
-            }
-        }); // end of addActionListener
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("stepContinuously");
+                    stepContinuously();
+                }
+            }); // end of addActionListener
 
         quitBtn.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                System.out.println("quit");
-                System.exit( 0 );
-            }
-        }); // end of addActionListener
+                public void actionPerformed(ActionEvent e){
+                    System.out.println("quit");
+                    System.exit( 0 );
+                }
+            }); // end of addActionListener
     } // addActionListenersToMyButtons( )
-    
-    
+
     /**
      * Initialize the delay slider and decorate it!
      */
@@ -256,18 +232,17 @@ public class LifeGame extends JApplet /* was Applet */ {
         delaySlider.setPaintLabels(true);
         return delayPanel;
     } /* buildDelayPanel( ) */
-    
-    
-/*
-Don't sub-components automatically get painted? 
+
+    /*
+    Don't sub-components automatically get painted? 
     public void paint( Graphics g ) {
-        this.setVisible( true); // necessary??
-        if ( theBoard != null ) {
-            theBoard.paint( g );
-            // theBoard.repaint( ); // which is better??
-        }
+    this.setVisible( true); // necessary??
+    if ( theBoard != null ) {
+    theBoard.paint( g );
+    // theBoard.repaint( ); // which is better??
+    }
     } // paint( )
-*/
+     */
 
     void newBoardFromFile(  ) {
         try {
@@ -284,32 +259,46 @@ Don't sub-components automatically get painted?
         repaint();
         stepContinuouslyBtn.setText("Run");
     } //newBoardFromFile
-    
-    
-	/**
-	* From Gavin 
-	* [ ] ??? We should have variable, not use button name as flag!
-	*/
+
+    /**
+     * From Gavin 
+     * [Ã] ??? We should have variable, not use button name as flag!
+     */
     void stepContinuously(){
-        if (stepContinuouslyBtn.getText().equals("Pause")) {
+        if (running) {
             stepContinuouslyBtn.setText("Run");
+            running = false;
         } else {
             stepContinuouslyBtn.setText("Pause");
+            running = true;
         }
     } /* stepContinuously( ) */
-    
-    
+
     /**
      * Tells the board to live life for a few turns.
      */
     void lifeStep ( int howManySteps ) {
         for ( int i = 0; i < howManySteps; ++i ) {
-                theBoard.step();
-                // theBoard.quickDraw( g );   // need to get g from somewhere!
-                //System.out.println( i + " " );;
+            theBoard.step();
+            // theBoard.quickDraw( g );   // need to get g from somewhere!
+            //System.out.println( i + " " );;
         } // for howManySteps
         repaint( );
     } // lifeStep()
 
+    /* Runnable Junk */
+    public void run() {
+        while(true) {
+            if (running) {
+                lifeStep(1);
+                try {
+                    stepperThread.sleep( delaySlider.getValue() );
+                } catch( Exception e ) {
+                    e.printStackTrace( System.err );
+                } /* end of try */                      
+            }
+            else stepperThread.yield();
+        } /* end of while */
+    } /* run( ) */
 
 } // Class LifeGame
